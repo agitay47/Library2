9459c8e067b8b6b67d8bb8bfd6c73333b7b1a247
Agit AY <47agitay@gmail.com>
2020-11-09 23:38:55 -0800

    Updated Library Project

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,10 +1,11 @@
 package com.cybertek.step_definitions;
 
-import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -29,9 +30,15 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
-        BrowserUtils.wait(5);
+    public void tearDown(Scenario scenario) {
+        //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
+        //this is a hook after
+        //runs automatically after every test
+        if (scenario.isFailed()) {
+            byte[] data = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
+
         Driver.closeDriver();
         System.out.println(":::(^_^) End of test execution (*_*):::");
-    }
-}
+    }}
