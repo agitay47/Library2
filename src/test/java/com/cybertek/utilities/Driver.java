5d3b2b84d143ef850965a7491fb161ad42a361d8
Agit AY <47agitay@gmail.com>
2020-11-10 21:29:25 -0800

    Updated Library Project

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+
 public class Driver {
     private static WebDriver driver;
+
     private Driver() {
     }
+
     public static WebDriver getDriver() {
         if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
@@ -45,12 +48,23 @@ public class Driver {
                         e.printStackTrace();
                     }
                     break;
+                case "remote-firefox":
+                    try {
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
+                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 default:
                     throw new RuntimeException("No such a browser yet!");
             }
         }
         return driver;
     }
+
     public static void closeDriver() {
         if (driver != null) {
             driver.quit();
