9755f8580fabfffd25b928732706d62aff52c9b5
Agit AY <47agitay@gmail.com>
2020-11-10 20:48:56 -0800

    Updated Library Project

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -3,28 +3,29 @@ package com.cybertek.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
-
 public class Driver {
-
-    //1-Make constructor private
-    private Driver(){
-    }
-
     private static WebDriver driver;
-
-    public static WebDriver getDriver(){
-
-        if (driver == null){
+    private Driver() {
+    }
+    public static WebDriver getDriver() {
+        if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
-
-            switch (browser){
+//            jenkins command: test -Dcucumber.filter.tags="@smoke" -Dbrowser="chrome"
+//            custom environment variables: -Dbrowser
+//            -Dproperty  = then read in java System.getProperty("property")
+//            if env variable was specified
+            if (System.getProperty("browser") != null) {
+//                then change browser type
+//                regardless on value configuration.properties
+                System.out.println("Browser type was changed to: " + System.getProperty("browser"));
+                browser = System.getProperty("browser");
+            }
+            switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
@@ -33,43 +34,27 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
-                case "chrome-headless":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
-                    break;
                 case "remote-chrome":
-
                     try {
-                        //   ChromeOptions chromeOptions = new ChromeOptions();
+//                    ChromeOptions chromeOptions = new ChromeOptions();
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
-                        // URL gridUrl = new URL("http://3.85.172.23:4444/wd/hub");
                         URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
-
                         driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-
                     break;
-                case "firefox-headless":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
-                    break;
-
+                default:
+                    throw new RuntimeException("No such a browser yet!");
             }
         }
-
         return driver;
-
     }
-
-    public static void closeDriver(){
-        if (driver != null){
+    public static void closeDriver() {
+        if (driver != null) {
             driver.quit();
-            driver=null;
+            driver = null;
         }
     }
-
-
 }
